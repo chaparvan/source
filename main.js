a6065eac500215a7d2829aa2a2eb8da686b43511
chaparvan <87499008+chaparvan@users.noreply.github.com>
2021-08-21 09:26:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let changeItem = (self, id) => {
   document.querySelector(id).classList.add("display-table-row-group");
 };
 function loader() {
-  setTimeout(showPage, 7000);
+  setTimeout(showPage, 10000);
 }
 
 function showPage() {
